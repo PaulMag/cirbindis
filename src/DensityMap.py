43ba6738694f4edf9e8ccb63e1aab85056dabe96
PaulMag <paulmag91@gmail.com>
2015-03-04 15:30:45 +0100

    Addde __init__ and load to DensityMap.

--- src/DensityMap.py
+++ src/DensityMap.py
@@ -1,4 +1,104 @@
+import sys
+    # For doing meta things like receiving command-line arguments and exiting
+    # the program.
+import numpy as np
+    #  Numerical Python. Contains mathematical functions for performing
+    # computations on arrays/matrices fast.
+import cPickle as pickle
+    # Can be used to save (dump) and load Python objects to files. Much
+    # faster than reading and writing ASCII tables.
+import time
+    # Used to time parts of code to look for bottlenecks.
+import matplotlib.pyplot as plt
+    # For plotting results.
+
+
+
 class DensityMap:
+
+
+    def __init__(self,
+        data=None,
+        filename=None,
+        radius_star=None,
+        radius_in=0,
+        radius_out=np.inf,
+    ):
+
+        if data is not None:
+            if data.shape[1] == 4
+                self.data = data
+            elif data.shape[1] == 3:
+                self.data = np.vstack((
+                    data[:, 0:2],
+                    np.zeros(data.shape[0]),
+                    data[:, 2, None],
+                ))
+
+        elif filename is not None:
+            self.data = self.load(filename)
+
+        self.data_rotated = None
+        self.radius_star = radius_star
+        self.radius_in = radius_in
+        self.radius_out = radius_out
+
+
+    def load(
+        filename,
+        method="pickle",
+        separator=" ",
+    ):
+        """Load a dataset to analyse from a file.
+
+        Assumed to be on the form 'x,y,density' or 'x,y,z,density' which
+        represents a point in cartesian space and the density at that point. If
+        the z-coordinate is not given it will be assumed to be 0 for every point.
+
+        filename: (string) Full pathname to file containing dataset.
+        radius_in: (float) Crop points that are closer than this to origo.
+        radius_out: (float) Crop points that are further than this to origo.
+        method: (string) What kind of loading algorithm to use.
+        separator: (string) If method='ascii' this is the separator
+            between the values each line. Usually a space or comma. Ignored if
+            method='pickle'.
+
+        return: (float, array) Array of shape (N, 4), where N is the number of
+            data points.
+        """
+
+        t_start = time.time()  # Just to time the loading, in case of large dataset.
+        infile = open(filename, "r")
+
+        if method == "pickle":
+            data = pickle.load(infile)
+            mask = (
+                (np.linalg.norm(data[:, 0:2], axis=1) >= self.radius_in) *
+                (np.linalg.norm(data[:, 0:2], axis=1) <= self.radius_out)
+            )
+            data = data[np.where(mask)]
+
+        elif method == "ascii":
+            data = []
+            for line in infile:
+                line = [float(value) for value in line.split(separator)]
+                if len(line) >= 3:
+                    if (self.radius_in <=
+                        np.linalg.norm(line[0:2]) <=
+                        self.radius_out
+                    ):
+                        data.append()
+            data = np.array(data)
+
+        infile.close()
+        t_end = time.time()  # End of timer.
+        print "Loading took %f seconds." % (t_end - t_start)
+
+        if data.shape[1] < 4:
+            # Add the z-dimension if it is not already there.
+            z = np.zeros((data.shape[0], 1))
+            data = np.hstack((data[:, 0:2], z, data[:, 2, None]))
+        self.data = data
 
 
     def rotate(self, angle_z=0, angle_y=0, angle_x=None, unit="deg"):
