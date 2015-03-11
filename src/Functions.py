7148f64f5beb140790714c2a17a14db6c182ad1e
PaulMag <paulmag91@gmail.com>
2015-03-11 17:12:07 +0100

    to_list doesn't handle strings as sequences.

--- src/Functions.py
+++ src/Functions.py
@@ -1,6 +1,11 @@
-def to_list(x):
+import numpy as np
+
+def to_list(x, dtype=None):
     try:
         iter(x)
+        if isinstance(x, basestring):
+            raise TypeError
+        x = np.array(x, dtype=dtype)
     except TypeError:
-        x = [x]
+        x = np.array([x], dtype=dtype)
     return x
