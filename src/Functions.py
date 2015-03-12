4f100332e47742f36a34171734a48df9f32a6a82
PaulMag <paulmag91@gmail.com>
2015-03-12 16:58:28 +0100

    Added pol2cart and cart2pol functions.

--- src/Functions.py
+++ src/Functions.py
@@ -10,3 +10,13 @@ def to_list(x, dtype=None, separator=" "):
         except TypeError:
             x = np.array([x], dtype=dtype)
     return x
+
+def cart2pol(x, y):
+    r = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    return [r, theta]
+
+def pol2cart(r, theta):
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+    return [x, y]
