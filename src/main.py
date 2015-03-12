c7c814ded54cf1315587eb9ed654910cf7147b4a
PaulMag <paulmag91@gmail.com>
2015-03-12 20:34:06 +0100

    radius_in can be a list, just like radius_out.

--- src/main.py
+++ src/main.py
@@ -10,25 +10,20 @@ if __name__ == "__main__":
     input_ = xmltodict.parse(infile)["input"]
     infile.close()
 
-    for radius_out in func.to_list(input_["radius_out"], float):
-        dataset = DensityMap(
-            filename=input_["datafile"],
-            inclinations=func.to_list(input_["inclination"], float),
-            radius_in=float(input_["radius_in"]),
-            radius_out=radius_out,
-            kappa=float(input_["kappa"]),
-        )
-        for star in func.to_list(input_["star"]):
-            dataset.add_star(star)
-        dataset.make_lightcurve(
-            n_angle=int(input_["azimuthsteps"]),
-            n_radius=int(input_["radiussteps"]),
-            unit=input_["unit"]["angle"],
-            show=True,
-        )
-        # dataset.set_r0()
-        # dataset.set_H()
-        # dataset.set_sigma()
-        # dataset.get_density_profile()
-
-    # dataset.writeto(filename)
+    for radius_in in func.to_list(input_["radius_in"], float):
+        for radius_out in func.to_list(input_["radius_out"], float):
+            dataset = DensityMap(
+                filename=input_["datafile"],
+                inclinations=func.to_list(input_["inclination"], float),
+                radius_in=radius_in,
+                radius_out=radius_out,
+                kappa=float(input_["kappa"]),
+            )
+            for star in func.to_list(input_["star"]):
+                dataset.add_star(star)
+            dataset.make_lightcurve(
+                n_angle=int(input_["azimuthsteps"]),
+                n_radius=int(input_["radiussteps"]),
+                unit=input_["unit"]["angle"],
+                show=True,
+            )
