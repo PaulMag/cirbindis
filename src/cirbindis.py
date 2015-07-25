cf46636a11f3307027caad629922168af28ce826
PaulMag <paulmag91@gmail.com>
2015-07-25 04:51:41 +0200

    Several H0 and diskmass can be provided at once in input.xml.
    User still has to press enter and view the plots between each run,
    so it is not faster at the moment. Need to streamline somehow.

--- src/cirbindis.py
+++ src/cirbindis.py
@@ -51,7 +51,6 @@ if __name__ == "__main__":
 
 
     for radius_in in func.to_list(input_["radius_in"], float):
-
         if radius_in is None or np.isnan(radius_in):
             radius_largest = 0.
             for stardict in func.to_list(input_["star"]):
@@ -62,43 +61,52 @@ if __name__ == "__main__":
             radius_in = radius_largest
 
         for radius_out in func.to_list(input_["radius_out"], float):
-
             if radius_out is None or np.isnan(radius_out):
                 radius_out = np.inf
 
-            dataset = DensityMap(
-                filename=input_["datafile"],
-                dataname=input_["dataname"],
-                coordsystem=input_["system"],
-                unit=input_["unit"],
-                inclinations=func.to_list(input_["inclination"], float),
-                radius_in=radius_in,
-                radius_out=radius_out,
-                diskmass=float(input_["diskmass"]),
-                diskradius=float(input_["diskradius"]),
-                H=float(input_["H0"]),
-                kappa=float(input_["kappa"]),
-            )
+            have_resaved = False
 
-            for filename in func.to_list(input_["resave_as"]):
-                if filename is None:
-                    continue
-                elif len(filename) == 0:
-                    continue
-                dataset.writeto(filename)
+            for H0 in func.to_list(input_["H0"], float):
+                for diskmass in func.to_list(input_["diskmass"], float):
+                    if len(func.to_list(input_["diskmass"], float)) > 1:
+                        dataname = input_["dataname"]+"__diskmass=%g" % diskmass
+                    else:
+                        dataname = input_["dataname"]
 
-            dataset.set_physical_units()
+                    dataset = DensityMap(
+                        filename=input_["datafile"],
+                        dataname=input_["dataname"],
+                        coordsystem=input_["system"],
+                        unit=input_["unit"],
+                        inclinations=func.to_list(input_["inclination"], float),
+                        radius_in=radius_in,
+                        radius_out=radius_out,
+                        diskmass=diskmass,
+                        diskradius=float(input_["diskradius"]),
+                        H=H0,
+                        kappa=float(input_["kappa"]),
+                    )
 
-            for star in func.to_list(input_["star"]):
-                dataset.add_star(star)
+                    if not have_resaved:
+                        for filename in func.to_list(input_["resave_as"]):
+                            if filename is None:
+                                continue
+                            elif len(filename) == 0:
+                                continue
+                            dataset.writeto(filename)
 
-            dataset.make_lightcurve(
-                n_angle=int(input_["azimuthsteps"]),
-                n_radius=int(input_["radiussteps"]),
-                unit=input_["unit"]["angle"],
-                save=True,
-                savefig=True,
-                show=True,
-                normalizations=func.to_list(input_["normalization"]),
-                outfolder=input_["outfolder"],
-            )
+                    dataset.set_physical_units()
+
+                    for star in func.to_list(input_["star"]):
+                        dataset.add_star(star)
+
+                    dataset.make_lightcurve(
+                        n_angle=int(input_["azimuthsteps"]),
+                        n_radius=int(input_["radiussteps"]),
+                        unit=input_["unit"]["angle"],
+                        save=True,
+                        savefig=True,
+                        show=True,
+                        normalizations=func.to_list(input_["normalization"]),
+                        outfolder=input_["outfolder"],
+                    )
